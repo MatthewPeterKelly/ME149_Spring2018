a1816ca88d713239273abc78d5083a15e2d94e1d
Matt Kelly <matthew.kelly2@gmail.com>
2018-02-07 22:26:49 -0500

    Updates for assignment four

---
+++ topics/04-tracking-control-basics/assignment/hw_04_studentName.m
@@ -0,0 +1,26 @@
+function hw_04_studentName()
+% Tufts ME 149 - Optimal Control for Robotics - HW 3
+%
+% Assignment 4:  Introduction to Tracking Controllers
+%
+% Assigned: Feb 6, 2018
+% Due: Feb 15, 2018
+%
+% Student Name:  TODO
+%
+% Collaborators:  TODO:
+%
+% References:  TODO
+%
+% How long did this assignment take you to complete?
+%   %%%% TODO
+%
+% Outline:
+%
+%   TODO
+%
+%
+
+%%%% TODO:  entry-point for simple and double pendulum simulations
+
+end
